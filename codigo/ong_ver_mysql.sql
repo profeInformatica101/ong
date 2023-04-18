f0f8bba2e23cbaf048e47076ec5b9b0a8214a4cb
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-04-18 10:49:16 +0200

    Update ong_ver_mysql.sql

--- codigo/ong_ver_mysql.sql
+++ codigo/ong_ver_mysql.sql
@@ -1,49 +1,49 @@
--- Crear la tabla ONG si no existe
-CREATE TABLE IF NOT EXISTS ONG (
-ID INT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR(255) NOT NULL,
-direccion VARCHAR(255) NOT NULL,
-telefono VARCHAR(20) NOT NULL,
-correo_electronico VARCHAR(255) NOT NULL,
-area_de_accion VARCHAR(255) NOT NULL
+-- Create ONG table
+CREATE TABLE ONG (
+ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+nombre TEXT NOT NULL,
+direccion TEXT NOT NULL,
+telefono TEXT NOT NULL,
+correo_electronico TEXT NOT NULL,
+area_de_accion TEXT NOT NULL
 );
 
--- Crear la tabla Voluntario si no existe
-CREATE TABLE IF NOT EXISTS Voluntario (
-ID INT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR(255) NOT NULL,
-apellido VARCHAR(255) NOT NULL,
+-- Create Voluntario table
+CREATE TABLE Voluntario (
+ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+nombre TEXT NOT NULL,
+apellido TEXT NOT NULL,
 fecha_de_nacimiento DATE NOT NULL,
-telefono VARCHAR(20) NOT NULL,
-correo_electronico VARCHAR(255) NOT NULL,
+telefono TEXT NOT NULL,
+correo_electronico TEXT NOT NULL,
 fecha_de_incorporacion DATE NOT NULL
 );
 
--- Crear la tabla Proyecto si no existe
-CREATE TABLE IF NOT EXISTS Proyecto (
-ID INT PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR(255) NOT NULL,
-descripcion VARCHAR(255) NOT NULL,
+-- Create Proyecto table
+CREATE TABLE Proyecto (
+ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+nombre TEXT NOT NULL,
+descripcion TEXT NOT NULL,
 fecha_de_inicio DATE NOT NULL,
 fecha_de_finalizacion DATE,
-estado VARCHAR(255) NOT NULL,
-ONG_responsable_ID INT,
+estado TEXT NOT NULL,
+ONG_responsable_ID INTEGER,
 FOREIGN KEY (ONG_responsable_ID) REFERENCES ONG(ID)
 );
 
--- Crear la tabla AsignacionVoluntariosONGs si no existe
-CREATE TABLE IF NOT EXISTS AsignacionVoluntariosONGs (
-voluntario_ID INT,
-ONG_ID INT,
+-- Create AsignacionVoluntariosONGs table
+CREATE TABLE AsignacionVoluntariosONGs (
+voluntario_ID INTEGER,
+ONG_ID INTEGER,
 PRIMARY KEY (voluntario_ID, ONG_ID),
 FOREIGN KEY (voluntario_ID) REFERENCES Voluntario(ID),
 FOREIGN KEY (ONG_ID) REFERENCES ONG(ID)
 );
 
--- Crear la tabla AsignacionVoluntariosProyectos si no existe
-CREATE TABLE IF NOT EXISTS AsignacionVoluntariosProyectos (
-voluntario_ID INT,
-proyecto_ID INT,
+-- Create AsignacionVoluntariosProyectos table
+CREATE TABLE AsignacionVoluntariosProyectos (
+voluntario_ID INTEGER,
+proyecto_ID INTEGER,
 PRIMARY KEY (voluntario_ID, proyecto_ID),
 FOREIGN KEY (voluntario_ID) REFERENCES Voluntario(ID),
 FOREIGN KEY (proyecto_ID) REFERENCES Proyecto(ID)
